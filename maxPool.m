5987d2b2ed191d175048dc31f0b77e0afd6c48bb
Hengjian Jia <henryjia18@yahoo.com>
2015-08-29 11:07:06 +0100

    Fixes maxPooling error and makes it work for multiple maps using cell arrays. Also changes test.m to test this.

--- maxPool.m
+++ maxPool.m
@@ -1,16 +1,17 @@
-function outputImgMat = maxPool(x, poolHeight, poolWidth)
+function outputMaps = maxPool(x, poolHeight, poolWidth, numIn)
 % note poolHeight and poolWidth must be factors of the dimensions of X
 
-numHeight = size(x, 1) / poolHeight;
-numWidth = size(x, 2) / poolWidth;
+numHeight = size(x{1}, 1) / poolHeight;
+numWidth = size(x{1}, 2) / poolWidth;
 
-output = zeros(numHeight, numWidth);
-
-for i = 1:numHeight
-  for j = 1:numWidth
-    %sample = x((i * poolHeight):((i + 1) * poolHeight - 1), (j * poolWidth):((j + 1) * poolWidth - 1));
-    sample = x(((i - 1) * poolHeight + 1):(i * poolHeight - 1), ((j - 1) * poolWidth + 1):(j * poolWidth - 1));
-    outputImgMat(i, j) = max(max(sample));
+for k = 1:numIn
+  outputMaps{k} = zeros(numHeight, numWidth);
+  for i = 1:numHeight
+    for j = 1:numWidth
+      %sample = x((i * poolHeight):((i + 1) * poolHeight - 1), (j * poolWidth):((j + 1) * poolWidth - 1));
+      sample = x{k}(((i - 1) * poolHeight + 1):(i * poolHeight), ((j - 1) * poolWidth + 1):(j * poolWidth));
+      outputMaps{k}(i, j) = max(max(sample));
+    end
   end
 end
 
--- test.m
+++ test.m
@@ -10,7 +10,7 @@ img{1} = rowToMat(x);
 
 layer1 = convForward(img, theta, bias, 1, 6);
 
-%layer2 = maxPool(layer1, 2, 2);
+layer2 = maxPool(layer1, 2, 2, 6);
 
 figure(1)
 imshow(img{1}, [min(min(img{1})), max(max(img{1}))])
@@ -18,7 +18,10 @@ for i = 1:6
   figure(i + 1)
   imshow(layer1{i}, [min(min(layer1{i})), max(max(layer1{i}))])
 end
-%figure(3)
-%imshow(layer2, [min(min(layer2{1})), max(max(layer2{1}))])
+
+for i = 1:6
+  figure(i + 6)
+  imshow(layer2{i}, [min(min(layer2{i})), max(max(layer2{i}))])
+end
 
 end
