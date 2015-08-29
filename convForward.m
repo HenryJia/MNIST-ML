2d01ba2d24b51525da7399382ea384956871ce84
Hengjian Jia <henryjia18@yahoo.com>
2015-08-29 10:46:36 +0100

    Makes convForward.m work for multiple maps using cell arrays. Also changes test.m to test this.

--- convForward.m
+++ convForward.m
@@ -1,22 +1,28 @@
-function outputImgMat = convForward(inputImgMat, theta, bias)
+function outputMaps = convForward(inputMaps, theta, bias, numIn, numOut)
 
-height = size(inputImgMat, 1) - (size(theta, 1) - 1);
-width = size(inputImgMat, 2) - (size(theta, 2) - 1);
+height = size(inputMaps{1}, 1) - (size(theta{1}, 1) - 1);
+width = size(inputMaps{1}, 2) - (size(theta{1}, 2) - 1);
+convHeight = size(theta{1}, 1) - 1;
+convWidth = size(theta{1}, 2) - 1;
 
-outputImgMat = zeros(height, width);
-
-for i = 1:height
-  for j = 1:width
-  outputImgMat(i, j) = sum(sum(theta .* inputImgMat(i:(i + 2), j:(j + 2))));
+for l = 1:numOut
+  outputMaps{l} = zeros(height, width);
+  for k = 1:numIn
+    for i = 1:height
+      for j = 1:width
+        outputMaps{l}(i, j) += sum(sum(theta{l} .* inputMaps{k}(i:(i + convHeight), j:(j + convWidth))));
+      end
+    end
   end
+  outputMaps{l} += bias(l);
+  % ReLU
+  outputMaps{l} = max(0, outputMaps{l});
 end
-
-outputImgMat += bias;
 
 % tanh activation
 %outputImgMat = tanh(outputImgMat);
 
-% RELU
-outputImgMat = log(1 + exp(outputImgMat));
+% softplus
+%outputImgMat = log(1 + exp(outputImgMat));
 
 end
--- test.m
+++ test.m
@@ -1,20 +1,24 @@
 function [img, layer1, layer2] = test(x)
 
-weights = randInitializeWeights(2, 3);
+for i = 1:6
+  theta{i} = randInitializeWeights(4, 5);
+end
 
-bias = randInitializeWeights(0, 1);
+bias = randInitializeWeights(5, 1);
 
-img = rowToMat(x);
+img{1} = rowToMat(x);
 
-layer1 = convForward(img, weights, bias);
+layer1 = convForward(img, theta, bias, 1, 6);
 
-layer2 = maxPool(layer1, 2, 2);
+%layer2 = maxPool(layer1, 2, 2);
 
 figure(1)
-imshow(img, [min(min(img)), max(max(img))])
-figure(2)
-imshow(layer1, [min(min(layer1)), max(max(layer1))])
-figure(3)
-imshow(layer2, [min(min(layer2)), max(max(layer2))])
+imshow(img{1}, [min(min(img{1})), max(max(img{1}))])
+for i = 1:6
+  figure(i + 1)
+  imshow(layer1{i}, [min(min(layer1{i})), max(max(layer1{i}))])
+end
+%figure(3)
+%imshow(layer2, [min(min(layer2{1})), max(max(layer2{1}))])
 
 end
