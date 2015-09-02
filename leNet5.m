6325321c592e88ed12c722a79a56cd3ac3d902fc
Hengjian Jia <henryjia18@yahoo.com>
2015-09-02 19:51:16 +0100

    Fully implements LeNet5.

--- leNet5.m
+++ leNet5.m
@@ -76,7 +76,7 @@ for i = 1:iters
   % Back propagate the errors with respect to the inputs of the MLP (the output of the convolutional layers
   convdelta4Row = (delta2 * Theta1')(:, 2:end);
   for j = 1:l3Dim
-    convdelta4(:, :, j) = rowToMat(convdelta4Row(1, ((i - 1) * 4 * 4 + 1):(i * 4 * 4)), 4, 4);
+    convdelta4(:, :, j) = rowToMat(convdelta4Row(1, ((j - 1) * 4 * 4 + 1):(j * 4 * 4)), 4, 4);
   end
 
   %convdelta4(:, :, 1:5)
@@ -90,13 +90,39 @@ for i = 1:iters
       end
     end
   end
-
   convdelta3 = convdelta3 .* (layer3 > 0);
+%{
+  convdelta2 = zeros(size(layer2));
+  for j = 1:l1Dim
+    for k = 1:l3Dim
+      filter = squeeze(convTheta2(:, :, j, k));
+      % Flip the feature matrix because of the definition of convolution, as explained later
+      %filter = rot90(squeeze(filter), 2);
+      %filter = squeeze(filter);
+      convdelta2(:, :, j) += conv2(squeeze(convdelta3(:, :, k)), filter, 'full');
+    end
+  end
+%}
+  convdelta2 = zeros(size(layer2));
+  for j = 1:l1Dim
+    for k = 1:l3Dim
+      convdelta2(:, :, j) += conv2(squeeze(convdelta3(:, :, k)), squeeze(convTheta2(:, :, j, k)), 'full');
+    end
+  end
+  convdelta1 = zeros(size(layer1));
+  for j = 1:l1Dim
+    for k = 1:12
+      for l = 1:12
+        convdelta1(((k - 1) * 2 + layer2Pos(1, k, l, j)), ((l - 1) * 2 + layer2Pos(2, k, l, j)), j) = convdelta2(k, l, j);
+      end
+    end
+  end
+  convdelta1 = convdelta1 .* (layer1 > 0);
 
   %convdelta3(:, :, 1)
   %layer3(:, :, 1)
 
-  % Back propagate the gradients with respect to convTheta2. This can be done by convoluting the layer before the weights with the errors of the layers after the weights
+  % Back propagate the gradients with respect to convTheta. This can be done by convoluting the layer before the weights with the errors of the layers after the weights
   for j = 1:l1Dim
     for k = 1:l3Dim 
       filter = convdelta3(:, :, k);
@@ -106,11 +132,22 @@ for i = 1:iters
       convDelta3(:, :, j, k) = conv2(squeeze(layer2(:, :, j)), filter, 'valid');
     end
   end
+  for k = 1:l1Dim 
+    filter = convdelta1(:, :, k);
+    % Flip the feature matrix because of the definition of convolution, as explained later
+    filter = rot90(squeeze(filter), 2);
+    %filter = squeeze(filter);
+    convDelta1(:, :, 1, k) = conv2(squeeze(x), filter, 'valid');
+  end
 
   % We don't need to backpropagate any more to get the gradients with respect to the biases as they're just convdelta3 summed
   for j = 1:l3Dim
-    convBiasDelta(j) = sum(sum(convdelta3(:, :, j)));
+    convBiasDelta2(j) = sum(sum(convdelta3(:, :, j)));
   end
+  for j = 1:l1Dim
+    convBiasDelta1(j) = sum(sum(convdelta1(:, :, j)));
+  end
+
   %convBias2 = convBias2 - convAlpha * convBiasDelta;
 
   % Apply gradients to stochastic gradient descent
@@ -119,10 +156,19 @@ for i = 1:iters
   %figure((i - 1) * iters + b)
   %imshow(convTheta2(:, :, 1), [min(min(convTheta2(:, :, 1))), max(max(convTheta2(:, :, 1)))])
 
-
+%{
   % For Gradient checking
   epsilon = 10^-5;
-  convTheta2(1, 2, 1, 1) += epsilon;
+  %convTheta2(1, 2, 4, 15) += epsilon;
+  %convTheta1(1, 2, 1, 4) += epsilon;
+  %layer4(1, 2, 15) += epsilon;
+  convBias1(2) += epsilon;
+
+  layer1 = cnnConvolve(5, l1Dim, x, convTheta1, convBias1);
+  layer1 = max(0, layer1);
+  for j = 1:l1Dim
+    [layer2(:, :, j), layer2Pos(:, :, :, j)] = maxPool(layer1(:, :, j), 2, 2);
+  end
 
   layer3 = cnnConvolve(5, l3Dim, layer2, convTheta2, convBias2);
   layer3 = max(0, layer3);
@@ -147,8 +193,15 @@ for i = 1:iters
   test = -y * log(a4)'
 
   (test - J((i - 1) * iters + b)) / epsilon
-  convDelta3(:, :, 1, 1)
-
+  %convDelta3(:, :, 4, 15)
+  %convdelta2(:, :, 1)
+  %convdelta4Row(1, 17:32)
+  %convdelta4(:, :, 2)
+  %convDelta1(:, :, 1, 4)
+  convBiasDelta1
+  %size(squeeze(convdelta3))
+  %size(squeeze(convdelta2))
+%}
   Theta3 = Theta3 - alpha * Delta3';
   Theta2 = Theta2 - alpha * Delta2';
   Theta1 = Theta1 - alpha * Delta1';
